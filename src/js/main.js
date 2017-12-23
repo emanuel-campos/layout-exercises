bf55f88429e4b94cfb21552f742327c6af58e8e6
Emanuel Campos <emanuelcampos.contato@gmail.com>
2017-12-23 09:50:10 -0300

    implementar carousel da pagina princiapl

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+(function($){
+	$(document).ready(function(){
+		/**
+		 * ativar o carousel principal da pagina inicial
+		 */
+		$('.rodar-galeria').owlCarousel({
+			items: 4
+		});
+	});
+})(jQuery)
